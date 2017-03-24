e3132926ff4ad511a573afb11ed0ee5af5c3b882
Stefan Siegl <stefan.siegl@mayflower.de>
2017-03-24 12:11:21 +0100

    Type FeatureSet.

--- src/FeatureSet.ts
+++ src/FeatureSet.ts
@@ -1,28 +1,28 @@
 import TrackingCollection from './utils/TrackingCollection';
 import Observable from './utils/Observable';
 
-export default class FeatureSet extends TrackingCollection {
-    constructor(...args) {
-        super(...args);
+export default class FeatureSet<T> extends TrackingCollection<T> {
+    constructor() {
+        super();
 
         this.observable.pointerEnter = new Observable();
         this.observable.pointerLeave = new Observable();
         this.observable.click = new Observable();
     }
 
-    _notifyPointerEnter(feature) {
+    _notifyPointerEnter(feature: T): void {
         if (this.contains(feature)) {
             this.observable.pointerEnter.fire(feature);
         }
     }
 
-    _notifyPointerLeave(feature) {
+    _notifyPointerLeave(feature: T): void {
         if (this.contains(feature)) {
             this.observable.pointerLeave.fire(feature);
         }
     }
 
-    _notifyClick(feature) {
+    _notifyClick(feature: T): void {
         if (this.contains(feature)) {
             this.observable.click.fire(feature);
         }
