86f2fff0ccce723a2c1a9d3c5bfaac327a5b7049
Christian Speckner <christian.speckner@mayflower.de>
2017-04-07 10:28:30 +0200

    Type GLFeatureSet + Quad.

--- src/Quad.ts
+++ src/Quad.ts
@@ -1,91 +1,104 @@
 import DependencyProvider from './utils/DependencyProvider';
 import Observable from './utils/Observable';
+import {Feature} from './FeatureSet';
+import RGBA from './RGBA';
 
-export default class Quad {
+export default class Quad implements Feature {
+
     constructor(
-        /* jshint ignore: start */
         {
             left    = 0,
             bottom  = 0,
             width   = 0,
             height  = 0,
             fillColor = null,
-        } = {}
-        /* jshint ignore: end */
+        }: {
+            left: number,
+            bottom: number,
+            width: number,
+            height: number,
+            fillColor: RGBA
+        }
     ) {
-        /* jshint ignore: start */
         this._left = left;
         this._bottom = bottom;
         this._width = width;
         this._height = height;
         this._fillColor = fillColor;
-        /* jshint ignore: end */
-        this.observable = {
-            change: new Observable()
-        };
-        this._dependencyProvider = new DependencyProvider(this);
 
         Observable.delegate(this, this.observable);
 
     }
 
-    _notifyChange() {
-        this._dependencyProvider.bump();
-        this.observable.change.fire();
-    }
-
-    setLeft(left) {
+    setLeft(left: number): this {
         this._left = left;
         this._notifyChange();
 
         return this;
     }
 
-    getLeft() {
+    getLeft(): number {
         return this._left;
     }
 
-    setBottom(bottom) {
+    setBottom(bottom: number): this {
         this._bottom = bottom;
         this._notifyChange();
 
         return this;
     }
 
-    getBottom() {
+    getBottom(): number {
         return this._bottom;
     }
 
-    setWidth(width) {
+    setWidth(width: number): this {
         this._width = width;
         this._notifyChange();
 
         return this;
     }
 
-    getWidth() {
+    getWidth(): number {
         return this._width;
     }
 
-    setHeight(height) {
+    setHeight(height: number): this {
         this._height = height;
         this._notifyChange();
 
         return this;
     }
 
-    getHeight() {
+    getHeight(): number {
         return this._height;
     }
 
-    setFillColor(color) {
+    setFillColor(color: RGBA): this {
         this._fillColor = color;
         this._notifyChange();
 
         return this;
     }
 
-    getFillColor() {
+    getFillColor(): RGBA {
         return this._fillColor;
     }
+
+    private _notifyChange(): void {
+        this._dependencyProvider.bump();
+        this.observable.change.fire();
+    }
+
+    observable = {
+        change: new Observable()
+    };
+
+    private _left: number;
+    private _bottom: number;
+    private _width: number;
+    private _height: number;
+    private _fillColor: RGBA;
+
+    private _dependencyProvider = new DependencyProvider(this);
 }
--- src/renderer/webgl/GlFeatureSet.ts
+++ src/renderer/webgl/GlFeatureSet.ts
@@ -1,43 +1,93 @@
 import DependencyTracker from '../../utils/DependencyTracker';
 import Program from './glutil/Program';
+import FeatureSet from '../../FeatureSet';
+import ProjectionMatrix from './ProjectionMatrix';
+import TransformationMatrix from './TransformationMatrix';
+import PickingColorManager from './PickingColorManager';
+import Quad from '../../Quad';
 import * as shader from './shader';
 import * as utils from '../../utils';
 
 export default class GlFeatureSet {
-    constructor(gl, featureSet, projectionMatrix, transformationMatrix) {
-        this._gl = gl;
-        this._featureSet = featureSet;
+    constructor(
+        private _gl: WebGLRenderingContext,
+        private _featureSet: FeatureSet,
+        private _projectionMatrix: ProjectionMatrix,
+        private _transformationMatrix: TransformationMatrix
+    ) {
+        this._init();
+    }
+
+    render(): this {
+        const gl = this._gl;
+
+        this._updateProjectionMatrix();
+        this._updateTransformationMatrix();
+        this._rebuildVertices();
+
+        this._rebindBuffers();
+        gl.drawArrays(gl.TRIANGLES, 0, this._featureSet.count() * 6);
+
+        return this;
+    }
+
+    renderPicking(pickingColorManager: PickingColorManager): void {
+        const gl = this._gl;
+
+        this._updateProjectionMatrix();
+        this._updateTransformationMatrix();
+        this._rebuildPickingVertices(pickingColorManager);
+
+        this._rebindPickingBuffers();
+        gl.drawArrays(gl.TRIANGLES, 0, this._featureSet.count() * 6);
+    }
+
+    destroy(): void {
+        const gl = this._gl;
+
+        this._program = utils.destroy(this._program);
+
+        if (this._vertexPositionBuffer) {
+            gl.deleteBuffer(this._vertexPositionBuffer);
+            this._vertexPositionBuffer = null;
+        }
+
+        if (this._vertexColorBuffer) {
+            gl.deleteBuffer(this._vertexColorBuffer);
+            this._vertexPositionBuffer = null;
+        }
+
+        if (this._pickingColorBuffer) {
+            gl.deleteBuffer(this._pickingColorBuffer);
+            this._pickingColorBuffer = null;
+        }
+    }
+
+    private _init(): void {
+        const gl = this._gl;
+
         this._program = new Program(gl, shader.vsh.feature, shader.fsh.feature);
 
-        this._dependencyTracker = new DependencyTracker();
-        this._pickingDependencyTracker = new DependencyTracker();
-
-        this._projectionMatrix = projectionMatrix;
-        this._transformationMatrix = transformationMatrix;
-
-        this._vertexPositions = null;
-        this._vertexColors = null;
-        this._pickingColors = null;
         this._vertexPositionBuffer = gl.createBuffer();
         this._vertexColorBuffer = gl.createBuffer();
         this._pickingColorBuffer = gl.createBuffer();
     }
 
-    _updateProjectionMatrix() {
+    private _updateProjectionMatrix(): void {
         this._dependencyTracker.update(this._projectionMatrix, () => {
             this._program.use((ctx) =>
                 ctx.uniformMatrix4fv('u_ProjectionMatrix', this._projectionMatrix.getMatrix()));
         });
     }
 
-    _updateTransformationMatrix() {
+    private _updateTransformationMatrix(): void {
         this._dependencyTracker.update(this._transformationMatrix, () => {
             this._program.use((ctx) =>
                 ctx.uniformMatrix4fv('u_TransformationMatrix', this._transformationMatrix.getMatrix()));
         });
     }
 
-    _rebuildVertices() {
+    private _rebuildVertices(): void {
         const gl = this._gl;
 
         this._dependencyTracker.update(this._featureSet, () => {
@@ -63,7 +113,7 @@ export default class GlFeatureSet {
         });
     }
 
-    _rebuildQuad(quad, i) {
+    private _rebuildQuad(quad: Quad, i: number): void {
         const vertexBase = i * 12,
             colorBase = i * 24,
             bottom = quad.getBottom(),
@@ -87,12 +137,12 @@ export default class GlFeatureSet {
 
         for (let j = 0; j < 6; j++) {
             for (let k = 0; k < 4; k++) {
-                this._vertexColors[colorBase + 4*j + k] = color[k];
+                this._vertexColors[colorBase + 4 * j + k] = color[k];
             }
         }
     }
 
-    _rebuildPickingVertices(pickingColorManager) {
+    private _rebuildPickingVertices(pickingColorManager: PickingColorManager): void {
         const gl = this._gl;
 
         this._rebuildVertices();
@@ -103,13 +153,12 @@ export default class GlFeatureSet {
                 const featureCount = this._featureSet.count(),
                     colorBufferLength = 24 * featureCount;
 
-
                 if (!this._pickingColors || this._pickingColors.length !== colorBufferLength) {
                     this._pickingColors = new Float32Array(colorBufferLength);
                 }
 
                 this._featureSet.forEach((quad, i) => {
-                    this._rebuildPickingQuad(quad, i, pickingColorManager);
+                    this._rebuildPickingQuad(quad as Quad, i, pickingColorManager);
                 });
 
                 gl.bindBuffer(gl.ARRAY_BUFFER, this._pickingColorBuffer);
@@ -118,7 +167,7 @@ export default class GlFeatureSet {
         );
     }
 
-    _rebuildPickingQuad(quad, i, pickingColorManager) {
+    private _rebuildPickingQuad(quad: Quad, i: number, pickingColorManager: PickingColorManager): void {
         const colorBase = i * 24,
             pickingColor = pickingColorManager.getColor(i);
 
@@ -128,12 +177,12 @@ export default class GlFeatureSet {
 
         for (let j = 0; j < 6; j++) {
             for (let k = 0; k < 4; k++) {
-                this._pickingColors[colorBase + 4*j + k] = color[k];
+                this._pickingColors[colorBase + 4 * j + k] = color[k];
             }
         }
     }
 
-    _rebindBuffers() {
+    private _rebindBuffers(): void {
         const gl = this._gl;
 
         this._program.use((ctx) => {
@@ -147,7 +196,7 @@ export default class GlFeatureSet {
         });
     }
 
-    _rebindPickingBuffers() {
+    private _rebindPickingBuffers(): void {
         const gl = this._gl;
 
         this._program.use((ctx) => {
@@ -161,48 +210,16 @@ export default class GlFeatureSet {
         });
     }
 
-    render() {
-        const gl = this._gl;
+        private _program: Program = null;
 
-        this._updateProjectionMatrix();
-        this._updateTransformationMatrix();
-        this._rebuildVertices();
+        private _dependencyTracker = new DependencyTracker();
+        private _pickingDependencyTracker = new DependencyTracker();
 
-        this._rebindBuffers();
-        gl.drawArrays(gl.TRIANGLES, 0, this._featureSet.count() * 6);
+        private _vertexPositions: Float32Array = null;
+        private _vertexColors: Float32Array = null;
+        private _pickingColors: Float32Array = null;
 
-        return this;
-    }
-
-    renderPicking(pickingColorManager) {
-        const gl = this._gl;
-
-        this._updateProjectionMatrix();
-        this._updateTransformationMatrix();
-        this._rebuildPickingVertices(pickingColorManager);
-
-        this._rebindPickingBuffers();
-        gl.drawArrays(gl.TRIANGLES, 0, this._featureSet.count() * 6);
-    }
-
-    destroy() {
-        const gl = this._gl;
-
-        this._program = utils.destroy(this._program);
-
-        if (this._vertexPositionBuffer) {
-            gl.deleteBuffer(this._vertexPositionBuffer);
-            this._vertexPositionBuffer = null;
-        }
-
-        if (this._vertexColorBuffer) {
-            gl.deleteBuffer(this._vertexColorBuffer);
-            this._vertexPositionBuffer = null;
-        }
-
-        if (this._pickingColorBuffer) {
-            gl.deleteBuffer(this._pickingColorBuffer);
-            this._pickingColorBuffer = null;
-        }
-    }
+        private _vertexPositionBuffer: WebGLBuffer = null;
+        private _vertexColorBuffer: WebGLBuffer = null;
+        private _pickingColorBuffer: WebGLBuffer = null;
 }
